7accec8cf77f2006654021f40dc4be515c65c0cf
lijianru <2219269374@qq.com>
2020-02-16 20:26:27 +0800

    add redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,9 +3,6 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 
 import rootReducer from './rootReducer'
-import { State as ExampleState } from './reducers/examples'
-
-export type RootState = ExampleState
 
 const middleware = applyMiddleware(thunk, createLogger())
 
