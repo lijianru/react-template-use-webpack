8be9d915ec8588d16096f34dbfe73202aa120f9d
lijianru <2219269374@qq.com>
2020-02-16 15:23:45 +0800

    update Home

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,20 +1,56 @@
 import * as React from 'react'
-import styles from './styles.scss'
-import { Button } from 'antd'
+import { Button, Table } from 'antd'
+import axios from 'axios'
+
+interface Props {}
+interface State {
+  list: any[]
+}
+
+const columns = [
+  {
+    title: 'title',
+    dataIndex: 'title',
+    key: 'title',
+  },
+  {
+    title: 'last_reply_at',
+    dataIndex: 'last_reply_at',
+    key: 'last_reply_at',
+  },
+  {
+    title: 'create_at',
+    dataIndex: 'create_at',
+    key: 'create_at',
+  },
+]
 
 @log
-class Home extends React.Component {
+class Home extends React.Component<Props, State> {
+  state = { list: [] }
+  getData() {
+    axios.get('https://cnodejs.org/api/v1/topics').then(resp => {
+      console.table(resp.data.data)
+      this.setState({ list: [...resp.data.data] })
+    })
+  }
+
   render() {
+    const { list } = this.state
+
     return (
       <div>
         <section>
-          <ul>
-            <li className={styles.test}>吃饭</li>
-            <li>睡觉</li>
-            <li>打豆豆</li>
-          </ul>
-          <Button>TEST</Button>
+          <Button
+            onClick={() => {
+              this.getData()
+            }}
+          >
+            TEST
+          </Button>
         </section>
+        // @ts-ignore
+        <Table columns={columns} dataSource={list} rowKey={item => item.id} />
       </div>
     )
   }
