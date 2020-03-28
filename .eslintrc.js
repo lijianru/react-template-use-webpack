a437ebc7abdb44892c67481e9e6e259bd0aa1254
lijianru <2219269374@qq.com>
2020-03-28 17:59:43 +0800

    add hooks lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,8 +16,10 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', 'react-hooks'],
   rules: {
+    'react-hooks/rules-of-hooks': 'error', // 检查 Hook 的规则
+    'react-hooks/exhaustive-deps': 'warn', // 检查 effect 的依赖
     'space-before-function-paren': [
       'error',
       {
